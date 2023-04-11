7c26540ca8e1b2c31b69d2a8ec7e112239e3870a
Carole Gérard <gerard.carole@skynet.be>
2023-04-11 14:50:13 +0200

    Exo 06 - correction

--- src/components/clock-hook/date/date.jsx
+++ src/components/clock-hook/date/date.jsx
@@ -1,31 +1,57 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+
+// const DateComponent = () => {
+
+//     const [date, setDate] = useState('');
+
+//     useEffect(() => {
+
+//         const months = [
+//         "janvier", "février", "mars", "avril", "mai", "juin", "juillet",
+//         "août", "septembre", "octobre", "novembre", "décembre"
+//         ];
+
+//         const today = new Date();
+//         const day = today.getDate().toString().padStart(2, '0');
+//         const month = months[today.getMonth()];
+//         const year = today.getFullYear();
+
+//         const formattedDate = `${day} ${month} ${year}`;
+//         setDate(formattedDate);
+
+//     }, []);
+
+//     return (
+//         <>
+//             <p>{date}</p>
+//         </>
+//     )
+// }
+
+// export default DateComponent;
+
+
+
+// -----------------------------------------------
+// -----------------------------------------------
+// ----------------  CORRECTION  ----------------
+// -----------------------------------------------
+// -----------------------------------------------
+
+
 
 const DateComponent = () => {
 
-    const [date, setDate] = useState('');
+    const today = new Date();
 
-    useEffect(() => {
-
-        const months = [
-        "janvier", "février", "mars", "avril", "mai", "juin", "juillet",
-        "août", "septembre", "octobre", "novembre", "décembre"
-        ];
-
-        const today = new Date();
-        const day = today.getDate().toString().padStart(2, '0');
-        const month = months[today.getMonth()];
-        const year = today.getFullYear();
-
-        const formattedDate = `${day} ${month} ${year}`;
-        setDate(formattedDate);
-
-    }, []);
+    const formattedDate = today.toLocaleDateString('fr-be', { day: '2-digit', month: 'long', year: 'numeric' });
 
     return (
         <>
-            <p>{date}</p>
+            <p>{formattedDate}</p>
         </>
     )
+
 }
 
 export default DateComponent;
--- src/components/clock-hook/time/time.jsx
+++ src/components/clock-hook/time/time.jsx
@@ -1,58 +1,115 @@
-import { useEffect } from "react";
-import { useState } from "react";
+// import { useEffect } from "react";
+// import { useState } from "react";
+
+
+// const TimeComponent = () => {
+
+//     const [value, setValue] = useState(new Date());
+//     const [hour, setHour] = useState(0);
+//     const [minute, setMinute] = useState(0);
+//     const [second, setSecond] = useState(0);
+
+//     useEffect(() => {
+//         const interval = setInterval(() => {
+//             const currentValue = 
+//             new Date().getHours() * 3600 + 
+//             new Date().getMinutes() * 60 + 
+//             new Date().getSeconds();
+//             setValue(currentValue);
+        
+//             const currentHour = Math.floor(currentValue / 3600);
+//             setHour(currentHour);
+
+//             const currentMinute = Math.floor((currentValue % 3600) / 60);
+//             setMinute(currentMinute);
+
+//             const currentSecond = Math.floor(currentValue % 3600 % 60);
+//             setSecond(currentSecond);
+//         }, 200);
+
+//         return () => clearInterval(interval);
+//     }, [])
+
+
+//     // ON INIT
+//     // useEffect(() => {
+//     //     // console.log('ON INIT');
+//     //     const interval = setInterval(() => {
+//     //         setValue((prevValue) => prevValue + 0.2);
+//     //     }, 200);
+        
+//     //     // ON DESTROY
+//     //     return () => {
+//     //         // console.log('ON DESTROY');
+//     //         clearInterval(interval);
+//     //     }
+//     // }, []);
+
+    
+//     const time = `${hour < 10 ? `0${hour}` : hour}:${minute < 10 ? `0${minute}` : minute}:${second < 10 ? `0${second}` : second}`;
+
+//     return (
+//         <>
+//             {/* <p>{time === '0:0:0' ? 'Loading ...' : time}</p> */}
+//             <p>{time}</p>
+//         </>
+//     )
+// }
+
+// export default TimeComponent;
+
+
+// -----------------------------------------------
+// -----------------------------------------------
+// ----------------  CORRECTION  ----------------
+// -----------------------------------------------
+// -----------------------------------------------
+
+import { useEffect, useState } from "react";
 
 
 const TimeComponent = () => {
 
-    const [value, setValue] = useState('');
-    const [hour, setHour] = useState('');
-    const [minute, setMinute] = useState('');
-    const [second, setSecond] = useState('');
+    const [time, setTime] = useState(new Date());
 
+    // SET INTERVAL
     useEffect(() => {
-        const interval = setInterval(() => {
-            const currentValue = 
-            new Date().getHours() * 3600 + 
-            new Date().getMinutes() * 60 + 
-            new Date().getSeconds();
-            setValue(currentValue);
-        
-            const currentHour = Math.floor(currentValue / 3600);
-            setHour(currentHour);
+        const timerId = setInterval(() => {
+            setTime(new Date())
+        }, 200);
 
-            const currentMinute = Math.floor((currentValue % 3600) / 60);
-            setMinute(currentMinute);
-
-            const currentSecond = Math.floor(currentValue % 3600 % 60);
-            setSecond(currentSecond);
-        }, 1000);
-
-        return () => clearInterval(interval);
+        return () => {
+            clearInterval(timerId);
+        }
     }, [])
 
 
-    // ON INIT
-    useEffect(() => {
-        // console.log('ON INIT');
-        const interval = setInterval(() => {
-            setValue((prevValue) => prevValue + 0.2);
-        }, 200);
-        
-        // ON DESTROY
-        return () => {
-            // console.log('ON DESTROY');
-            clearInterval(interval);
-        }
-    }, []);
 
-    
-    const time = `${hour < 10 ? `0${hour}` : hour}:${minute < 10 ? `0${minute}` : minute}:${second < 10 ? `0${second}` : second}`;
+    // SET TIMEOUT
+    // useEffect(() => {
+    //     const timerId = setTimeout(() => {
+    //         setTime(new Date())
+    //     }, 200);
+
+    //     return () => {
+    //         clearInterval(timerId);
+    //     }
+    // })
+
+
+    const hours = time.getHours().toString().padStart(2, 0)
+    const minutes = time.getMinutes().toString().padStart(2, 0)
+    const secondes = time.getSeconds().toString().padStart(2, 0)
+
 
     return (
+
         <>
-            <p>{time === '0:0:0' ? 'Loading ...' : time}</p>
+            <p>{hours} : {minutes} : {secondes}</p>
         </>
     )
+
 }
+
 
 export default TimeComponent;
